b1faad5f326f8754381503b792690c61c43e7e43
AntonioNP <AntonioNP@AntonioNP-PC>
2014-01-07 16:26:56 +0100

    Creación de la clase ProductoBean.java

---
+++ src/main/java/net/rafaelaznar/bean/ProductoBean.java
@@ -0,0 +1,61 @@
+package net.rafaelaznar.bean;
+
+/**
+ *
+ * @author AntonioNP
+ */
+public class ProductoBean {
+
+    private int id;
+    private String codigo, descripcion, precio;
+    private int id_tipoproducto;
+
+    public ProductoBean() {
+
+    }
+
+    public ProductoBean(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public int getId_tipoproducto() {
+        return id_tipoproducto;
+    }
+
+    public void setId_tipoproducto(int id_tipoproducto) {
+        this.id_tipoproducto = id_tipoproducto;
+    }
+
+}
